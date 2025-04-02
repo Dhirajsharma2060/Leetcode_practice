1caa97586f1fc6fa86b738d64187bacbe9a5ac9d
Dhiraj Sharma <147904725+Dhirajsharma2060@users.noreply.github.com>
2025-04-02 20:39:00 +0530

    Time: 2 ms (39.97%), Space: 18.2 MB (15.08%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n=len(nums)
+        ans,sol=[],[]
+        def backtrack():
+            #yaha pe first hamne check kiya ki agar 
+            if len(sol)==n:
+                #iska matlab ki hamra jho nums me value hai vho pura ho gaya hai 
+                ans.append(sol[:])
+                return 
+            #matlab ki jab array me se number liya hai vho agar sol me hai to mat lena    
+            for x in nums:
+                if x  not in sol:
+                    sol.append(x)
+                    backtrack()
+                    sol.pop()
+        backtrack()
+        return ans                 
+
+
+
+        
