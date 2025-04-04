6184e0216b12c13aaa06515e59b73050398413ec
Dhiraj Sharma <147904725+Dhirajsharma2060@users.noreply.github.com>
2025-04-04 20:56:00 +0530

    Time: 15 ms (63.74%), Space: 17.7 MB (88.78%) - LeetHub

---
+++ 3799-unique-3-digit-even-numbers/3799-unique-3-digit-even-numbers.py
@@ -0,0 +1,13 @@
+from typing import List
+from itertools import permutations
+class Solution:
+    def totalNumbers(self, digits: List[int]) -> int:
+        unique_numbers=set()
+        for p in permutations(digits,3):
+            if p[0]==0:
+                continue
+            if p[2]%2==0:
+                num=p[0]*100+p[1]*10+p[2]
+                unique_numbers.add(num)
+        return len(unique_numbers)            
+
