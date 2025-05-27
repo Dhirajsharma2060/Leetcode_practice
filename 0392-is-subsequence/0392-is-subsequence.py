1265a6c9325d711928e8cb4a3c9050fabdb1b097
Dhiraj Sharma <147904725+Dhirajsharma2060@users.noreply.github.com>
2025-05-27 17:40:38 +0530

    Time: 5 ms (3.78%), Space: 19.2 MB (5.1%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,14 +1,18 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        s_len=len(s)
-        t_len=len(t)
-        i,j=0,0
-        while i<s_len and j<t_len:
+        def helper(i,j):
+            if i==len(s):
+                return True
+            #iska matlab ki t katam ho gaya hai  but  s baki hai 
+            if j==len(t):
+                return False
             if s[i]==t[j]:
-                i+=1
-            j+=1    
+                return helper(i+1,j+1)
+            else:
+                return helper(i,j+1)
+        return helper(0,0)                
 
-        return i==len(s)  
+
 
 
 
