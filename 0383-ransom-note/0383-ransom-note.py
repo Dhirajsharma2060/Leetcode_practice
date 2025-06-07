37fe2fae52b7bac4f4c09a83a2dc751b390ab850
Dhiraj Sharma <147904725+Dhirajsharma2060@users.noreply.github.com>
2025-06-08 00:06:05 +0530

    Time: 15 ms (77.15%), Space: 18.2 MB (13.78%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -4,6 +4,12 @@ class Solution:
         ransomCounter=Counter(ransomNote)
         magazineCounter=Counter(magazine)
         for char in ransomCounter:
+            # iska malab ko jab ransomNote ke count value jiyada hoga magazine se then retuen
+            #FALSE 
+            #example  ransomNote = "aa", magazine = "ab"
+            #iska ransome count is {a:2} and magazine ka {a:1 , b:1}
+            #because we are checking for individual character so just a then ransomnote ka value 
+            # more then magazine ka count so False
             if ransomCounter[char]>magazineCounter[char]:
                 return False
         return True         
