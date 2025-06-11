de6862184104ed42f0c71f5cdba5e64c4e61094e
Dhiraj Sharma <147904725+Dhirajsharma2060@users.noreply.github.com>
2025-06-11 22:53:43 +0530

    Time: 2 ms (53.48%), Space: 18.9 MB (51.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h={}
         for i in range(len(nums)):
+            #example 3,2,3,1
+            #so hash map should be {3:0,2:1,3:2,1:3}
+            #but if we do h[nums[i]]=i then {3:2,2:1,1:3}
+            h[nums[i]]=i
+        for i in  range(len(nums)):
             val=target-nums[i]
-            for j in range(i+1,len(nums)):
-                if nums[j]==val:
-                    return [i,j]
-        return [0,0]        
-        
+            if val in h and h[val]!=i:
+                return [h[val],i]
+
+
+        #time complexity is o(n^2)
+        #space complexity is o(n)
