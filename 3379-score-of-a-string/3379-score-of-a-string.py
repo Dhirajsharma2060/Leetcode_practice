f054d6d96008bdc067456435dcbb2bd26579a20b
Dhiraj Sharma <147904725+Dhirajsharma2060@users.noreply.github.com>
2025-11-16 09:55:29 +0530

    Time: 0 ms (100%), Space: 17.9 MB (30.04%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.py
+++ 3379-score-of-a-string/3379-score-of-a-string.py
@@ -1,9 +1,7 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        total=0
-        # loop is liye len(s)-1 tak ki jho chratoer each loop me s[i] and s[i+1] agar last gaya to last chracter ke bad bhi number dundega jho milega nahi liye 
+        res=0
         for i in range(len(s)-1):
-            total+=abs(ord(s[i])-ord(s[i+1]))
-        return total    
-        
+            res+=abs(ord(s[i])-ord(s[i+1]))
+        return res    
         
